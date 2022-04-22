73932a93b89150f59f587d022d0ef12967c85796
Ben Coomes <bencoomes@github.com>
2022-04-22 12:18:39 +0000

    Set root page to articles view and update articles view.

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
 end
